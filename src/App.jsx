9b7f3c1621337bb79d18315065886c5d46b1b962
Giorgia <giorgia.giro@gmail.com>
2025-01-18 00:59:17 +0200

    before api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
+import Header from "./components/Header"
+import PokeCard from "./components/PokeCard"
+import SideNav from "./components/SideNav"
 
 function App() {
-  
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <SideNav />
+            <PokeCard />
+        </>
+    )
 }
 
 export default App
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react"
+import { first151Pokemon, getFullPokedexNumber } from "../utils"
 
 export default function SideNav() {
-  return (
-    <nav>SideNav</nav>
-  )
+    return (
+        <nav>
+            <div className={"header"}>
+                <h1 className="text-gradient">Pokédex</h1>
+            </div>
+            <input type="text" placeholder="Search" />
+            {first151Pokemon.map((pokemon, pokemonIndex) => {
+                return (
+                    <button className={"nav-card "} key={pokemonIndex}>
+                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        <p>{pokemon}</p>
+                    </button>
+                )
+            })}
+        </nav>
+    )
 }
