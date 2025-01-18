6e461c0f1b938a56a8095dadb96f7ef457c7f293
Giorgia <giorgia.giro@gmail.com>
2025-01-18 13:02:02 +0200

    modal base

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 
 export default function Modal() {
-  return (
-    <div>Modal</div>
+  return ReactDom.createPortal(
+    <div className="modal-container">
+      <button onClick={handleCloseModal} className="modal-underlay" />
+
+      <div className="modal-content">
+        {children}
+      </div>
+    </div>,
+    //find that id of portal
+    document.getElementById('portal')
   )
 }
